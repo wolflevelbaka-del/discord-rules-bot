084a5b3367fd258f7d5ca6cebae567db8155c8e8
wolflevelbaka-del <wolflevelbaka@gmail.com>
2026-01-03 20:26:32 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 require("dotenv").config();
+
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds]
+});
+
+client.once("ready", () => {
+  console.log(`✅ Eingeloggt als ${client.user.tag}`);
+});
+
+client.login(process.env.TOKEN);
